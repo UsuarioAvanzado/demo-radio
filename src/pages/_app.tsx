367bf8e60955e454db37e7fd62ee6282a632d5fb
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2021-11-29 17:48:20 -0300

    build: move "pages" and update tsconfig
    Move "pages" directory to "src" directory and
    add paths mapping to tsconfig.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss'
+import '@/styles/globals.scss'
 import "reset-css"
 import type { AppProps } from 'next/app'
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import styles from '@/styles/Home.module.scss'
+
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Open radio</title>
         <meta name="description" content="Frontend demo using radio-browser API " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-
+          Main page
       </main>
 
       <footer className={styles.footer}>
