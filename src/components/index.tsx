1a8a4556b699d9dc1fe9702724dd95cf561a48ed
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2021-12-21 14:55:28 -0300

    feat(*.tsx): add index components

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import { Component as MediaPlayer } from "./MediaPlayer"
+import { Component as StationBlock } from "./StationBlock"
+import { Component as Loading } from "./Loading"
+import { Component as Head } from "./Head";
+
+export {
+  MediaPlayer,
+  StationBlock,
+  Loading,
+  Head
+}
