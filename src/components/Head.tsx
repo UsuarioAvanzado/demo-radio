c98cd3e6b3de5dc822df062e3297b5a65cbbc88e
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2021-12-21 16:13:12 -0300

    feat(*.tsx): add Head component

---
+++ src/components/Head.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export function Component() {
+  return (
+      <Head>
+        <title>Radio</title>
+        <meta name="description" content="Frontend demo using radio-browser API " />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  )
+}
