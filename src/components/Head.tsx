b7e1a1c4fc415ba65395d616042c813a9c5eae6b
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2021-12-26 16:57:40 -0300

    feat(*.tsx): add meta viewport tag

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,6 +5,7 @@ export function Component() {
       <Head>
         <title>Radio</title>
         <meta name="description" content="Frontend demo using radio-browser API " />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
   )
