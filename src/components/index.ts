7a08510f8a206448cb327d461da5b161fb0cf845
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2021-12-28 02:22:01 -0300

    refactor: rename and update components index
    rename index.tsx to index.ts and update
    components exports.

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Component as MediaPlayer } from "./MediaPlayer"
+export { Component as StationBlock } from "./StationBlock"
+export { Component as Loading } from "./Loading"
+export { Component as Head } from "./Head";
+export { Component as ColorSchemaToggle  } from "./ColorSchemaToggle"
