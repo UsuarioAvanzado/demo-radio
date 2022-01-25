03d0bc1d7b36f9cc43e2983f84781cef50ff28a1
John <66654720+UsuarioAvanzado@users.noreply.github.com>
2022-01-25 15:39:36 -0300

    refactor: adapt StationBlock for selectors system

--- src/components/StationBlock/src/component.tsx
+++ src/components/StationBlock/src/component.tsx
@@ -1,18 +1,34 @@
-import { useState, useRef, useEffect } from 'react'
 import type { Station } from "radio-browser-api";
-import style from './style.module.scss'
-
-
+import styles from './style.module.scss';
+import { useStoreDispatch , useStoreState} from "@hooks";
+import { selectors } from "@store";
+import type { MediaPlayerState } from '@store'
 
 export function Component( station: Station ) {
-  return (
-    <div className={style.wrapper}>
+  const dispatch = useStoreDispatch()
+  const mediaState = useStoreState<MediaPlayerState>(selectors.mediaPlayer)
 
-      <section className={style.info}>
-        <span>name: {station.name}</span>
+ 
+  function handler(){
+   if(mediaState.station && mediaState.station.id == station.id) {
+     dispatch({type: '@media/toggle'})
+   } 
+   else {
+     dispatch({type: '@media/start', payload: station})
+   }
+  }
+
+  return (
+    <div className={`${styles.wrapper} shadow`}>
+      <section className={styles.info}>
+        <span className={`content ${styles.content}`}>{station.name}</span>
+        <span className={`content ${styles.content}`}>{station.urlResolved}</span>
+        <span className={`content ${styles.content}`}>radio info</span>
       </section>
-      <section className={style.control}>
-        control
+      <section className={styles.control}>
+        <span onClick={handler} className={styles.image_container}>
+          <img src={station.favicon} className={styles.image} alt="logo"/>
+        </span>
       </section>
     </div>
   )
